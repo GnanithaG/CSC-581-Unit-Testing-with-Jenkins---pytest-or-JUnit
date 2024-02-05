85c08f0dd3e12cbd03477efcd55863add3a95d87
Gnanitha G <lguttikonda1@toromail.csudh.edu>
2024-02-04 22:24:27 -0800

    MathUtil test

---
+++ test_MathUtils.py
@@ -0,0 +1,13 @@
+import MathUtils
+
+def test_add():
+    assert MathUtils.MathUtils().add() == 5
+
+def test_divide():
+    assert MathUtils.MathUtils().divide() == -1.0
+
+def test_times():
+    assert MathUtils.MathUtils().times() == 0
+
+def test_substract():
+    assert MathUtils.MathUtils().substract() == 5
